29da8421f7e473d0dfe88b08aa98179d3e490288
Viktor Johansson <viktor.krabba@gmail.com>
2017-09-12 12:35:01 +0200

    Adds scores/{id} endpoint to access single scores

---
+++ api/Controllers/ScoresController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ScoreApi.Data;
+using ScoreApi.Models;
+
+namespace ScoreApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class ScoresController : Controller
+    {
+        private readonly ScoreContext _context;
+
+        public ScoresController(ScoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetScore(long id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            // TODO: See if the user has access to this score's leaderboard.
+            var score = await _context.Scores.FirstOrDefaultAsync(s => s.Id == id);
+
+            if (score == null)
+            {
+                return NotFound();
+            }
+
+            return new JsonResult(score);
+        }
+    }
+}
