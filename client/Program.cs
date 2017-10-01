69dcab430d4a1576026823d62bbe46bb2f2fcfd6
Viktor Johansson <viktor.krabba@gmail.com>
2017-10-01 16:48:57 +0200

    Implements client program to request api with auth

--- client/Program.cs
+++ client/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
 
 namespace Client
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "ro.client", "secret");
+            var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync("test@test.com", "Testtest!1", "score_api_v1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/api/v1/leaderboards/1");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(JArray.Parse(content));
+            }
         }
     }
 }
